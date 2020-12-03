a782c2eb3670ba00e1994dbb5848882a09c32b14
Ryan Wang <1070623719@qq.com>
2020-12-03 21:10:36 +0800

    Refactor: config & logger

--- SingleTable/myLogger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def getMyLogger():
-    logging.basicConfig(level = logging.DEBUG,filename='./log.log',format = '%(asctime)s - %(levelname)s - %(message)s')
-    filehandler = logging.FileHandler('./log.log',encoding='utf-8')
-    logging.getLogger().addHandler(filehandler )
-    logger = logging.getLogger()
-    return logger
